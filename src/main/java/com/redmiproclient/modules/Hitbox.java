562e5ae67c044957056244d2f59594288787d256
m85thegoat <scammerhunter92@gmail.com>
2026-03-29 12:12:12 +0800

    Add Hitbox module with onTick method

---
+++ src/main/java/com/redmiproclient/modules/Hitbox.java
@@ -0,0 +1,13 @@
+package com.redmiproclient.modules;
+
+public class Hitbox extends Module {
+    public Hitbox() {
+        super("Hitbox");
+    }
+
+    @Override
+    public void onTick() {
+        if (!isEnabled()) return;
+        // Requires mixin – placeholder
+    }
+}
