ef5fbb1c13cef2646834fb13ba2647825f2ed769
m85thegoat <scammerhunter92@gmail.com>
2026-03-29 12:12:40 +0800

    Add Reach module with onTick method

---
+++ src/main/java/com/redmiproclient/modules/Reach.java
@@ -0,0 +1,13 @@
+package com.redmiproclient.modules;
+
+public class Reach extends Module {
+    public Reach() {
+        super("Reach");
+    }
+
+    @Override
+    public void onTick() {
+        if (!isEnabled()) return;
+        // Requires mixin – placeholder
+    }
+}
